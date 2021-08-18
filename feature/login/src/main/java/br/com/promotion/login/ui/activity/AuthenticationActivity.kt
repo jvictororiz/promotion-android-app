364f6f84208f75402576a9a306bb51a7d6e13224
Karinasantos <karina.bsb.x@hotmail.com>
2021-08-18 09:46:46 -0300

    adjsutments

--- feature/login/src/main/java/br/com/promotion/login/ui/activity/AuthenticationActivity.kt
+++ feature/login/src/main/java/br/com/promotion/login/ui/activity/AuthenticationActivity.kt
@@ -2,6 +2,7 @@ package br.com.promotion.login.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.com.promotion.firebaseservice.userServiceModule
 import br.com.promotion.login.R
 import br.com.promotion.login.di.authenticationModule
 import br.com.promotion.ui_component.di.commonModule
@@ -12,11 +13,11 @@ class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
-        loadKoinModules(listOf(authenticationModule, commonModule))
+        loadKoinModules(listOf(authenticationModule, commonModule, userServiceModule))
     }
 
     override fun onDestroy() {
-        unloadKoinModules(listOf(authenticationModule, commonModule))
+        unloadKoinModules(listOf(authenticationModule, commonModule, userServiceModule))
         super.onDestroy()
     }
 }
--- firebaseService/src/main/java/br/com/promotion/firebaseservice/UserServiceModule.kt
+++ firebaseService/src/main/java/br/com/promotion/firebaseservice/UserServiceModule.kt
@@ -1,8 +1,10 @@
 package br.com.promotion.firebaseservice
 
 import br.com.promotion.firebaseservice.service.UserServiceImpl
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val userServiceModule = module {
