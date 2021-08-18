6f6dd9e3fb662ef53a8e5260505069796f37b3aa
jvictororiz <joao.victor.roriz@hotmail.com>
2021-08-17 22:12:03 -0300

    module service user

---
+++ firebaseService/src/main/java/br/com/promotion/firebaseservice/FirebaseModule.kt
@@ -0,0 +1,24 @@
+package br.com.promotion.firebaseservice
+
+import br.com.promotion.firebaseservice.service.UserServiceImpl
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import org.koin.dsl.module
+
+
+val userServiceModule = module {
+    factory {
+        FirebaseAuth.getInstance()
+    }
+    factory {
+        FirebaseFirestore.getInstance().document(DATABASE_USER)
+    }
+    factory<UserService> {
+        UserServiceImpl(
+            authentication = get(),
+            database = get()
+        )
+    }
+}
+
+private const val DATABASE_USER = "users"
