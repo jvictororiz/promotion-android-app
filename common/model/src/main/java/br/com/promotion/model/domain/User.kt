126e8905d60cadf2f9d9e5358a3f6170e5b21618
Karinasantos <karina.bsb.x@hotmail.com>
2021-08-17 21:10:52 -0300

    resolve conflits login

--- common/model/src/main/java/br/com/promotion/model/domain/User.kt
+++ common/model/src/main/java/br/com/promotion/model/domain/User.kt
@@ -1,9 +1,7 @@
 package br.com.promotion.model.domain
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-
 data class User(
     val name: String = "",
-    val email: String="",
-    val password:String=""
+    val email: String = "",
+    val password: String = ""
 )
